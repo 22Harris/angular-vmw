a333fabf8086e5c070e6f9f1d2732142e9e388bc
Harris <harris@dreamslab.fr>
2025-07-15 13:48:36 +0300

    [Navbar] make style on step

--- src/app/libs/navbar-component/navbar-component.ts
+++ src/app/libs/navbar-component/navbar-component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-component',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar-component.html',
   styleUrl: './navbar-component.css'
 })
 export class NavbarComponent {
-
+  router = inject(Router);
 }
