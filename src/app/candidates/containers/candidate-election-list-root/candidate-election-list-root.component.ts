ddd430039ba5625d260570031b79293e138d4ae9
Harris <harris@dreamslab.fr>
2025-07-16 11:24:46 +0300

    [Candidates] make user interface on list of posts

--- src/app/candidates/containers/candidate-election-list-root/candidate-election-list-root.component.ts
+++ src/app/candidates/containers/candidate-election-list-root/candidate-election-list-root.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ListOfElectionComponentComponent } from '../../components/list-of-election-component/list-of-election-component.component';
 
 @Component({
   selector: 'app-candidate-election-list-root',
-  imports: [],
+  imports: [ListOfElectionComponentComponent],
   templateUrl: './candidate-election-list-root.component.html',
   styleUrl: './candidate-election-list-root.component.css'
 })
